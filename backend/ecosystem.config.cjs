877e7db715a979c0e676e8010bce32229eaff932
DongNV <dongnv@avada.email>
2025-11-17 11:00:29 +0700

    clean code

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'a2-avada-image-backend',
+      name: 'avada-image-backend',
       script: './server.js',
       instances: 1,
       exec_mode: 'fork',
